9b0cd83bed2a18ea501f5767354e1e8a86db6eaf
Elisa Barontini <nirinproductions@gmail.com>
2021-08-24 22:38:50 +0100

    all iterations completed

--- index.js
+++ index.js
@@ -12,16 +12,79 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
+  .then((recipe) => {
+    return Recipe.create({
+      title: 'Rigatoni alla Genovese',
+      cuisine: 'Italian',
+      duration: 200
+    });
+  })
+  .then((recipe) => {
+    console.log(`Recipe was successfully added!`);
+  })
+  .then((recipes) => {
+    return Recipe.insertMany([
+      {
+        title: 'Tagliata alla fiorentina',
+        cuisine: 'Italian',
+        enum: 'main_course'
+      },
+      {
+        title: 'Sushi',
+        cuisine: 'Japanese',
+        enum: 'main_course'
+      },
+      {
+        title: 'Schnitzel',
+        cuisine: 'Austrian',
+        enum: 'main_course'
+      },
+
+      {
+        title: 'Carrot Cake',
+        cuisine: 'British',
+        enum: 'dessert',
+        duration: 40
+      }
+    ]);
+  })
+
+  .then((recipe) => {
+    console.log(`All the recipes were successfully added!`);
+  })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.findOneAndUpdate(
+      'Rigatoni alla Genovese',
+      {
+        duration: 100
+      },
+      { new: true }
+    );
   })
-  .catch(error => {
+
+  .then(() => {
+    console.log(`The recipes was successfully updated!`);
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+
+  .then(() => {
+    console.log(`The recipes was successfully removed!`);
+    return mongoose.disconnect();
+  })
+
+  .then(() => {
+    console.log('Connection has been destroyed');
+  })
+
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,68 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true
+  },
+
+  level: {
+    type: String,
+    enum: ['EasyPeasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+
+  ingredients: {
+    type: [String]
+  },
+
+  cuisine: {
+    type: String,
+    required: true
+  },
+
+  dishType: {
+    type: String,
+    enum: [
+      'breakfast',
+      'main_course',
+      'soup',
+      'snack',
+      'drink',
+      'dessert',
+      'other'
+    ]
+  },
+
+  image: {
+    type: String,
+    value: ['EasyPeasy', 'Amateur Chef', 'UltraPro Chef'],
+    default: function () {
+      if (this.image) {
+        return (location.href =
+          'https://images.media-allrecipes.com/images/75131.jpg');
+      }
+      return null;
+    }
+  },
+
+  duration: {
+    type: Number,
+    minduration: 0
+  },
+
+  creator: {
+    type: String
+  },
+
+  created: {
+    type: Date
+    /*default: function () {
+      if (this.released) {
+        return Date.now();
+      }
+      return null;
+    }*/
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
